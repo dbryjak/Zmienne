2e00420c3f1877115a58d27b70434dc92cf2b912
DanielBryjak <d.bryjak@op.pl>
2022-11-01 15:24:56 +0100

    Tworzenie zmiennych w jednej linijce, lub tworzone zmienne - kazda w  osobnej linijce. Jak wprowadzać wartości zmiennych.

---
+++ src/Zmienne_Dodatek.java
@@ -0,0 +1,17 @@
+public class Zmienne_Dodatek {
+    public static void main(String[] args) {
+    // każda nazwa zmiennej wprowadzona w osobnej linijce
+    int nazwaZmiennej = 10;
+    int a;
+    int b;
+    int c = 5;
+
+    a = 10;                 // wprowadzenie wartości zmiennej
+    nazwaZmiennej = 11;     // wprowadzenie wartości zmiennej
+
+    // np. te same zmienne wprowadzone w jednej linijce
+    int d, e = 5, nazwaZmiennejDwa = 5;
+
+
+    }
+}
