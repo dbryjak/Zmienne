debdafc7ab768a820647e8d0874f74f0b7551b38
DanielBryjak <d.bryjak@op.pl>
2022-11-01 14:51:28 +0100

    Tworzenie Zmiennych - Typy Proste: Liczby całkowite: byte, short, int, long. Liczby zmiennoprzecinkowe: float, double. Pojedyńcze znaki: char. Wartości logiczne: boolean.

--- src/Typy_Proste.java
+++ src/Typy_Proste.java
@@ -21,6 +21,24 @@ public class Typy_Proste {
         System.out.println("Long: " + zmiennaLong);
         System.out.println(Long.MIN_VALUE + " " + Long.MAX_VALUE);
 
+        // liczby zmiennoprzecinkowe
 
+        float zmiennaFloat = 12.34123456789f;
+        System.out.println("Float: " + zmiennaFloat);
+        // System.out.println(Float.MIN_VALUE + " " + Float.MAX_VALUE);
+
+        double zmiennaDouble = 12.34123456789;
+        System.out.println("Double: " + zmiennaDouble);
+
+        // float - 32 bity - mniej precyzynje - 6 miejsc po przecinku
+        // double - 64 bity - bardziej precyzyjne - 11 miejsc po przecinku
+
+        // Pojedyńcze znaki
+        char zmiennaChar = 'A';
+        System.out.println("Char: " + zmiennaChar);
+
+        // Wartości logiczne
+        boolean zmiennaBoolean = false;
+        System.out.println("Boolean: " + zmiennaBoolean);
     }
 }
