453d1819197040280b5e524e65a7927fe10509f5
DanielBryjak <d.bryjak@op.pl>
2022-11-01 17:17:06 +0100

    Wartości domyślne dla Typow Prostych

---
+++ src/Typy_Proste_Wartosci_Domyslne.java
@@ -0,0 +1,25 @@
+public class Typy_Proste_Wartosci_Domyslne {
+
+    public static int domyslnyInt;
+    public static float domyslnyFloat;
+    public static char domyslnyChar;
+    public static boolean domyslnyBoolean;
+
+    public static void main(String[] args) {
+        /*
+        byte        0
+        short       0
+        int         0
+        long        0L
+        float       0f
+        double      0.0d
+        char        '\u0000'
+        boolean     false
+        */
+
+        System.out.println("domyslnyInt: " + domyslnyInt);
+        System.out.println("domyslnyFloat: " + domyslnyFloat);
+        System.out.println("domyslnyChar: " + domyslnyChar);
+        System.out.println("DomyslnyBoolean: " + domyslnyBoolean);
+    }
+}
