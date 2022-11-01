6d53f28813a5f161ce3015c9a3a500d9e9aa1cb5
DanielBryjak <d.bryjak@op.pl>
2022-11-01 11:09:16 +0100

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,20 @@
+public class Zmienne {
+    public static void main(String[] args) {
+        // ang. variables - Zmienne
+
+        // typZmiennej nazwaZmiennej;
+
+        int dowolnaNazwaDlaInteger; // canelCase - każda Nastepna Nazwa Z Duzej Litery;
+
+        int liczbaOkienWDomku; // deklaracja zmiennej, bez podanej wartości
+        liczbaOkienWDomku = 30;
+        liczbaOkienWDomku = 32; // modyfikacja zmiennej
+
+
+        int LiczbaDrzwiWDomku = 10; // inicjalizacja zmiennej, (przypisana wartosci)
+
+
+
+    }
+
+}
